85cd8e82da76bf6bbc289116b47677e7743fb6d6
FieryCod <kwcharllie379@gmail.com>
2018-07-07 21:46:40 +0200

    feat(docker): Add alias for docker-compose in development

--- project.clj
+++ project.clj
@@ -9,17 +9,19 @@
                  [ring/ring-defaults "0.3.2"]
                  [ring-logger "1.0.1"]]
 
-  :source-paths ["src"]
-
-
-  :plugins [[lein-ring "0.12.4"]
-            [lein-bikeshed "0.5.1"]
-            [lein-kibit "0.1.6"]
-            [jonase/eastwood "0.2.7"]]
+  :source-paths ["src"] :plugins [[lein-ring "0.12.4"]
+                                  [lein-bikeshed "0.5.1"]
+                                  [lein-kibit "0.1.6"]
+                                  [lein-shell "0.5.0"]
+                                  [jonase/eastwood "0.2.7"]]
 
   :ring {:handler codcheck.server/runnable-server
          :adapter {:min-threads 5 :max-threads 150}
          :async?  false}
+
   :aliases {"ci-check" ["do" ["kibit"] ["eastwood"] ["bikeshed"]]}
+
   :profiles {:dev {:dependencies [[javax.servlet/javax.servlet-api "4.0.1"]
-                                  [ring/ring-mock "0.3.0"]]}})
+                                  [ring/ring-mock "0.3.0"]]
+                   :aliases {"dc-up" ["do" ["shell"
+                                            "docker-compose" "-f" "docker/docker-compose.development.yml" "up"]]}}})
